ce1caa3b1915808c750aca6759a7e7c9e3b97e70
AgrawalVaishvi <vaishviagrawal05@gmail.com>
2025-12-12 13:37:32 +0530

    string api

--- lect21/hello.java
+++ lect21/hello.java
@@ -1,24 +1,19 @@
-//runnable keyword ka use karke thread crreate karna
-import java.io.*;
+//string api 
+import java.util.ArrayList;
+import java.util.List;
 
-class myThread implements Runnable{
-    public void run (){
-       for(int i = 0;i<5 ; i++){
-        System.out.println(i);
-         try{
-                Thread.sleep(2000);
-       }
-       catch(Exception e){
-        System.out.println(e);
-       }
-    }
-}
-}
 class hello{
     public static void main(String[] args) {
-        myThread task = new myThread();
-        Thread t1 = new Thread (task);
-        t1.start();
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        List<Integer> res = list.stream().filter(n -> n >20).toList();
+        List<Integer> mapRes = list.stream().map(n -> n *2).toList();
+        int data = list.stream().reduce(0, (a, b) -> a + b);
+        System.out.println(res);
+        System.out.println(mapRes); 
+        System.out.println(data);
     }
 }
